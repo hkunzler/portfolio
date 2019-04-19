3d0cf8124309757891e7693ae18a3424dc1db3ea
Harmony <hkunzler@gmail..com>
2019-04-18 21:40:20 -0600

    Tidying code

--- app.js
+++ app.js
@@ -26,36 +26,27 @@ document.addEventListener('DOMContentLoaded', function () {
   var instances = M.Carousel.init(elems);
 });
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   var elems = document.querySelectorAll(".sidenav");
-//   var instances = M.Sidenav.init(elems, options);
-// });
-
-
 $(document).ready(function(){
   var $sections = $('.container-section');
-  
+
   $(window).scroll(function(){
-    
+
     var currentScroll = $(this).scrollTop() + $(window).height();
     console.log(currentScroll)
     var $currentSection
-    
+
     $sections.each(function(){
       var divPosition = $(this).offset().top;
-      
+
       if( divPosition + 1 < currentScroll ){
         $currentSection = $(this);
-        
+
       }
-      
+
       var id = $currentSection.attr('id');
    	 $('a').removeClass('active-nav');
    	 $("[href=#"+id+"]").addClass('active-nav');
-      
-    })
 
+    })
   });
 });
-
-
